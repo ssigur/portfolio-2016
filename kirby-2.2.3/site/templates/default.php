7ad5ce715980c86ef211f4eb392d4c44f3a82601
Stéphane SIGUR <stephane@sigur.net>
2016-05-03 11:16:55 +0200

    page error
    redirection temporisé vers page d'accueil

--- kirby-2.2.3/site/templates/default.php
+++ kirby-2.2.3/site/templates/default.php
@@ -5,6 +5,17 @@
     <div class="text">
       <h1><?php echo $page->title()->html() ?></h1>
       <?php echo $page->text()->kirbytext() ?>
+
+      <?php if($page->isErrorPage()):
+        // redirection temporisé vers page d'accueil
+        ?>
+        <script type="text/javascript">
+          <!--
+          var obj = 'window.location.replace("<?php echo $page->parent()->url() ?>");';
+          setTimeout(obj,4000);
+          // -->
+        </script>
+      <?php endif ?>
     </div>
 
   </main>
