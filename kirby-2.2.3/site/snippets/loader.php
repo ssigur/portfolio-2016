4ef6a5c9ed60ad38a670679a71754369665d720e
Stéphane SIGUR <stephane@sigur.net>
2016-05-04 21:16:53 +0200

    [Refactoring] Loader

--- kirby-2.2.3/site/snippets/loader.php
+++ kirby-2.2.3/site/snippets/loader.php
@@ -1,1 +1,11 @@
+<span class="loader loader-circles"></span>
+
+<?php /*
+<span class="loader loader-quart"></span>
+<span class="loader loader-double"></span>
+<span class="loader loader-circles"></span>
 <span class="loader loader-bars"><span></span></span>
+<span class="loader loader-bubbles">
+  <span></span>
+</span>
+ */ ?>
